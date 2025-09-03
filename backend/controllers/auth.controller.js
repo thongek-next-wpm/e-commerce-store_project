85eb929dd26ed4207f5211689d4007898d18e976
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-09-03 14:12:01 +0700

    backend setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("sign up router called");
+};
+
+export const login = async (req, res) => {
+  res.send("login up router called");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout router called");
+};
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//router
+import authRouters from "./routers/auth.route.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5002;
+
+//authentication
+app.use("/api/auth", authRouters);
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
