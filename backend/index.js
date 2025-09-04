e2514c002e48eb8ec8df31cea4668e9adb3e37d2
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-09-04 13:58:46 +0700

    change post

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./lib/database.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 6379;
 
 //allows you to parse the body of the reqest
 app.use(express.json());
