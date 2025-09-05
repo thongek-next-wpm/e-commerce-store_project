826ab0dad4fbc4267205614c29f5112e06af1160
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-09-05 16:09:00 +0700

    Login Refresh&Access tokens

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -70,7 +70,29 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login up router called");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    console.log("Here runs the login:", email);
+
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
