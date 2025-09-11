f8b71a878d5cdf64f5ae0fea7a80d6e76ae79ede
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-09-11 14:21:47 +0700

    set post product and add clouinary

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import { redis } from "../lib/redis.js";
+import cloudinary from "../lib/cloudinary.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -34,6 +35,32 @@ export const getFeaturedProducts = async (req, res) => {
     res.json(featuredProducts);
   } catch (error) {
     console.log("Error in getFeaturedProducts", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+    let cloudinaryResponse = null;
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      impage: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
+
+    res.status(201).json({ message: "Product created", product });
+  } catch (error) {
+    console.log("Error in createProduct", error.message);
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
--- backend/routers/product.route.js
+++ backend/routers/product.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAllProducts,
   getFeaturedProducts,
+    createProduct,
 } from "../controllers/product.controller.js";
 import { productsRoute, adminRoute } from "../middleware/auth.middleware.js";
 
@@ -9,6 +10,6 @@ const router = express.Router();
 
 router.get("/", productsRoute, adminRoute, getAllProducts);
 router.get("/feature", getFeaturedProducts);
-
+router.post("/", productsRoute, adminRoute, createProduct);
 
 export default router;
