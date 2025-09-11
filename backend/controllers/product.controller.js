2f1eccaed588c37b67cfe9dcf112728884a59ca6
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-09-11 12:56:24 +0700

    setup feature

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 export const getAllProducts = async (req, res) => {
@@ -6,6 +7,33 @@ export const getAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.log("Error in getAllProducts", error.Product);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // If not in cache, fetch from database
+    // .lean() returns plain JavaScript objects instead of Mongoose documents
+    // This improves performance when you don't need Mongoose document methods
+    // wich is good for read-only operations like this one
+
+    featuredProducts = await Product.find({ isfeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" });
+    }
+
+    // Store in Redis cache for future requests
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in getFeaturedProducts", error.message);
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
--- backend/routers/product.route.js
+++ backend/routers/product.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { productsRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", productsRoute, adminRoute, getAllProducts);
+router.get("/feature", getFeaturedProducts);
+
 
 export default router;
