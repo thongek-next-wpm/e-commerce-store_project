175a1588628afdb29045a3cc053a7bc556e5e5c0
thongek-next-wpm <thongek_wpm@hotmail.com>
2025-09-12 14:40:12 +0700

    addToCart

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,9 +1,21 @@
+//backend/controllers/cart.controller.js
+
 export const addToCart = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    const { productId } = req.body;
+    const userId = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      user.cartItems.productId.push(productId);
     }
+    await user.save();
+    res.json(user.cartItems);
+  } catch (error) {
+    console.log("Error adding to cart:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 export const getCartProducts = async (req, res) => {};
 export const removeAllFromCart = async (req, res) => {};
